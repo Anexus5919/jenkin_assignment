3f6a870ddd22966dec23db6b11868259a67e4af5
Anexus5919 <anexus5919@gmail.com>
2025-09-27 00:49:50 +0530

    Update test_01.java

--- test_01.java
+++ test_01.java
@@ -1,5 +1,5 @@
-public clas test_01 {
+public class test_01 {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Hello, World!")
     }
 }
