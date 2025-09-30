dd3a8eea079f34d80634612ae98f9f3dcd4bc639
Anexus5919 <anexus5919@gmail.com>
2025-10-01 00:34:21 +0530

    Update JavaCode.java

--- JavaCode.java
+++ JavaCode.java
@@ -1,1 +1,5 @@
-console.log('Hello World, this is my most basic Java code!');
+public class JavaCode {
+    public static void main(String[] args) {
+        System.out.println("Hello World, this is my most basic Java code!");
+    }
+}
